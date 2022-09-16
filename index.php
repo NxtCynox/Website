063a29c8efe458b3100cac47f17155c6dd2744c9
NxtCynox <80179882+NxtCynox@users.noreply.github.com>
2022-09-16 09:58:03 +0200

    Update and rename Index.php to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>My first PHP Website</title>
+    </head>
+    <body>
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back<br/><br/>
+        <form action="register.php" method="POST">
+           Enter Username: <input type="text" 
+           name="username" required="required" /> <br/>
+           Enter password: <input type="password" 
+           name="password" required="required" /> <br/>
+           <input type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
+  
+  <?php
+
+
+?>
